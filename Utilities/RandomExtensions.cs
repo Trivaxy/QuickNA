32d7395f54ac4c51ed7f22f9f8f753d558451b39
Trivaxy <itsmeomar2003@yahoo.com>
2021-07-06 11:18:02 +0300

    few more utils

--- Utilities/RandomExtensions.cs
+++ Utilities/RandomExtensions.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 
 namespace QuickNA.Utilities
 {
 	public static class RandomExtensions
 	{
+		public static float NextFloat(this Random random) => (float)random.NextDouble();
+
 		public static float NextFloat(this Random random, float minValue, float maxValue)
 		{
-			return (float)random.NextDouble() * (maxValue - minValue) + minValue;
+			return random.NextFloat() * (maxValue - minValue) + minValue;
+		}
+
+		public static Vector2 NextVector2(this Random random, float minRotation = 0f, float maxRotation = (float)Math.PI * 2)
+		{
+			float angle = minRotation + maxRotation * random.NextFloat();
+			return new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
 		}
 	}
 }
---
+++ Utilities/Vector3Extensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace QuickNA.Utilities
+{
+	public static class Vector3Extensions
+	{
+		public static Vector2 Flatten(this Vector3 vector) => new Vector2(vector.X, vector.Y);
+	}
+}
