6b03533acd921fb891e0017a732ffd4298d9c261
Trivaxy <itsmeomar2003@yahoo.com>
2021-08-05 17:36:27 +0300

    Transform + RenderText component

---
+++ ECS/Components/Render.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using QuickNA.Assets;
+using QuickNA.Rendering;
+
+namespace QuickNA.ECS.Components
+{
+	public struct Render
+	{
+		public Handle<RenderLayer> RenderLayerHandle;
+		public Handle<Texture2D> TextureHandle;
+		public Color Color;
+		public Rectangle? SourceRectangle;
+		public SpriteEffects SpriteEffects;
+		public Vector2 Origin;
+
+		public Render(Handle<RenderLayer> renderLayerHandle, Handle<Texture2D> textureHandle)
+		{
+			RenderLayerHandle = renderLayerHandle;
+			TextureHandle = textureHandle;
+			Color = Color.White;
+			SourceRectangle = null;
+			SpriteEffects = SpriteEffects.None;
+			Origin = Vector2.Zero;
+		}
+
+		public Render(Handle<RenderLayer> renderLayerHandle, Handle<Texture2D> textureHandle, Color color)
+		{
+			RenderLayerHandle = renderLayerHandle;
+			TextureHandle = textureHandle;
+			Color = color;
+			SourceRectangle = null;
+			SpriteEffects = SpriteEffects.None;
+			Origin = Vector2.Zero;
+		}
+
+		public Render(Handle<RenderLayer> renderLayerHandle, Handle<Texture2D> textureHandle, Color color, Rectangle sourceRectangle, SpriteEffects spriteEffects, Vector2 origin)
+		{
+			RenderLayerHandle = renderLayerHandle;
+			TextureHandle = textureHandle;
+			Color = color;
+			SourceRectangle = sourceRectangle;
+			SpriteEffects = spriteEffects;
+			Origin = origin;
+		}
+	}
+}
---
+++ ECS/Components/Transform.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace QuickNA.ECS.Components
+{
+	public struct Transform
+	{
+		public Vector2 Position;
+		public Vector2 Scale;
+		public float Rotation;
+
+		public Transform(Vector2 position)
+		{
+			Position = position;
+			Scale = Vector2.One;
+			Rotation = 0f;
+		}
+
+		public Transform(Vector2 position, Vector2 scale)
+		{
+			Position = position;
+			Scale = scale;
+			Rotation = 0f;
+		}
+
+		public Transform(Vector2 position, float rotation)
+		{
+			Position = position;
+			Scale = Vector2.One;
+			Rotation = rotation;
+		}
+
+		public Transform(Vector2 position, Vector2 scale, float rotation)
+		{
+			Position = position;
+			Scale = scale;
+			Rotation = rotation;
+		}
+	}
+}
